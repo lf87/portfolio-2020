40c36cb65ec63b50aea9b5879c649430a3b1bedb
lf87 <luke.fryer.email@gmail.com>
2020-07-26 14:44:24 +0100

    git commit -m 'first commit'

---
+++ gridsome.config.js
@@ -0,0 +1,37 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+const path = require('path')
+
+function addStyleResource (rule) {
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        path.resolve(__dirname, './src/assets/scss/utils/*.scss'),
+      ],
+    })
+}
+
+module.exports = {
+  siteName: 'Gridsome',
+  plugins: [],
+  siteUrl: 'https://lf20.github.io',
+  pathPrefix: '/lf20',
+  chainWebpack (config) {
+    // Load variables for all vue-files
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+
+    types.forEach(type => {
+      addStyleResource(config.module.rule('sass').oneOf(type))
+    })
+
+    // or if you use scss
+    types.forEach(type => {
+      addStyleResource(config.module.rule('scss').oneOf(type))
+    })
+  }
+}
